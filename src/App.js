337c7e71c205917d6c00b576f5d2c41f89a987ac
Pouya Karimi <pouya.karimi@farmersinsurance.com>
2018-05-05 17:09:54 -0700

    first powerball ticket

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from './component/header/header'
+import Footer from './component/footer/footer'
+import {Button} from 'react-bootstrap'
+import GenerateNumber from './component/generatenumber/GenerateNumber'
 
 class App extends Component {
+  test = () => {
+    alert("hello");
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container border border-primary">
+        <Header></Header>
+        <div class="container border border-primary" onClick={this.test}>
+          <Button bsStyle="primary">click me</Button>
+        </div>
+        <GenerateNumber/>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/component/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+    render() {
+        return (
+            <div class="container bg-success border border-primary">
+                <h2>This is the header.</h2>
+            </div>
+        );
+    }
+}
+
+export default header;
