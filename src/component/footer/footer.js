d49282faa683b2fd579413890207897b2ca198df
pouya06 <pouya06@gmail.com>
2018-05-10 16:18:22 -0700

    make hundred tickets (#2)

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class footer extends Component {
     render() {
         return (
-            <div class="container bg-warning border border-primary">
+            <div className="container bg-warning border border-primary">
                 <h2>This is the footer.</h2>
             </div>
         );
